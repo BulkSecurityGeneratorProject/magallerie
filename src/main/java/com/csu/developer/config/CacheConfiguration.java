266c39fcd645b64b75f513f5b0d8e0c2a28bd2e4
csu6 <csuwannapak@gmail.com>
2018-11-10 11:47:49 +0100

    make it better

--- src/main/java/com/csu/developer/config/CacheConfiguration.java
+++ src/main/java/com/csu/developer/config/CacheConfiguration.java
@@ -38,6 +38,11 @@ public class CacheConfiguration {
             cm.createCache(com.csu.developer.domain.User.class.getName(), jcacheConfiguration);
             cm.createCache(com.csu.developer.domain.Authority.class.getName(), jcacheConfiguration);
             cm.createCache(com.csu.developer.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(com.csu.developer.domain.Album.class.getName(), jcacheConfiguration);
+            cm.createCache(com.csu.developer.domain.Photo.class.getName(), jcacheConfiguration);
+            cm.createCache(com.csu.developer.domain.Photo.class.getName() + ".tags", jcacheConfiguration);
+            cm.createCache(com.csu.developer.domain.Tag.class.getName(), jcacheConfiguration);
+            cm.createCache(com.csu.developer.domain.Tag.class.getName() + ".photos", jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
